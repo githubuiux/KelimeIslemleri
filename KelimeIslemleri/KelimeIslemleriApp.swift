0129f359a3d3b37c8010564def72f8e0c2255c72
githubuiux <uiuxista@gmail.com>
2023-05-08 12:47:57 +0300

    Initial Commit

---
+++ KelimeIslemleri/KelimeIslemleriApp.swift
@@ -0,0 +1,17 @@
+//
+//  KelimeIslemleriApp.swift
+//  KelimeIslemleri
+//
+//  Created by Levent on 8.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct KelimeIslemleriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
